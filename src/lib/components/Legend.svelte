aded5ae0ac384450510dc71620454837227b5c5e
Adhavan <adhavan.sivaraj16@gmail.com>
2025-08-07 15:44:44 +0530

    Toggle all layers in the internal maplibre layer including all layers containing the name of the layer. Possible bug! parts of a name can be non-unique and will change visibility settings for all similarly named layers.

--- src/lib/components/Legend.svelte
+++ src/lib/components/Legend.svelte
@@ -1,57 +1,60 @@
 <script lang="ts">
 	export let map;
 	export let data;
-	function handleVisibilityChange(layer: any) {
+
+	function toggleNlayersVisibility(layer) {
 		if (map) {
-			map.setLayoutProperty(
-				layer.name + '_layer',
-				'visibility',
-				layer.visible ? 'visible' : 'none'
-			);
+			const allLayers = map.getStyle().layers;
+			const nlayers = allLayers.filter((l) => l.id.toLowerCase().includes(layer.name.toLowerCase()));
+
+			nlayers.forEach((layer) => {
+				console.log(map.getLayoutProperty(layer.id, 'visibility'))
+				map.setLayoutProperty(layer.id, 'visibility', map.getLayoutProperty(layer.id, 'visibility')=='none' ? 'visible' : 'none');
+			});
 		}
 	}
 </script>
 
-<section id="legend" class="md:max-h-screen flex flex-col gap-10 p-4">
-<div class="">
-	<a class="text-xs/6" href="/">From the Participatory Geospatial Lab.</a>
-	<h1 class="font-bold">{data.content.name}</h1>
-	<p class="my-2 text-xs">{data.content.short_desc}</p>
-</div>
+<section id="legend" class="flex flex-col gap-10 p-4 md:max-h-screen">
+	<div class="">
+		<a class="text-xs/6" href="/">From the Participatory Geospatial Lab.</a>
+		<h1 class="font-bold">{data.content.name}</h1>
+		<p class="my-2 text-xs">{data.content.short_desc}</p>
+	</div>
 	<div class="overflow-y-scroll">
-	{#each data.content.layers as layer}
-		<div id="legend-item ">
-			<div class="my-2 grid grid-cols-5 rounded-sm bg-neutral-300 p-2">
-				<div class="col-span-4 text-left">
-					<h3>{layer.name}</h3>
-					<p class="text-xs">
-						{layer.description}
-					</p>
-					{#if layer.export_url}
-						{#if layer.export_url.csv}
-							<a href={layer.export_url.csv} target="_blank">Download CSV</a>
+		{#each data.content.layers as layer}
+			<div id="legend-item ">
+				<div class="my-2 grid grid-cols-5 rounded-sm bg-neutral-300 p-2">
+					<div class="col-span-4 text-left">
+						<h3>{layer.name}</h3>
+						<p class="text-xs">
+							{layer.description}
+						</p>
+						{#if layer.export_url}
+							{#if layer.export_url.csv}
+								<a href={layer.export_url.csv} target="_blank">Download CSV</a>
+							{/if}
+							{#if layer.export_url.geojson}
+								| <a href={layer.export_url.geojson} target="_blank">Download GeoJSON</a>
+							{/if}
 						{/if}
-						{#if layer.export_url.geojson}
-							| <a href={layer.export_url.geojson} target="_blank">Download GeoJSON</a>
-						{/if}
-					{/if}
-					<span></span>
-				</div>
-				<div class="my-auto px-5 align-middle">
-					<input
-						type="checkbox"
-						id={layer.name}
-						bind:checked={layer.visible}
-						onchange={() => {
-							handleVisibilityChange(layer);
-						}}
-						aria-checked={layer.visible}
-					/>
+						<span></span>
+					</div>
+					<div class="my-auto px-5 align-middle">
+						<input
+							type="checkbox"
+							id={layer.name}
+							bind:checked={layer.visible}
+							onchange={() => {
+								toggleNlayersVisibility(layer);
+							}}
+							aria-checked={layer.visible}
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
-	{/each}
-</div>
+		{/each}
+	</div>
 </section>
 
 <style lang="postcss">
